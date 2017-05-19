bf99f0379a2e94bf4cba2433192eb08975ba7326
Ryan Fleck <ryan.fleck@protonmail.com>
2017-05-19 12:39:49 -0400

    Scripted sync.

--- Java/RCF.DataStructureTests/src/TreeNode.java
+++ Java/RCF.DataStructureTests/src/TreeNode.java
@@ -7,12 +7,20 @@
  *
  */
 public class TreeNode {
-	private TreeNode left;
-	private TreeNode right;
-	private TreeNode prev;
+	private TreeNode left=null,right=null,prev=null;
 	private String info;
+	private Object storedObject;
 	
+	public TreeNode(){}
 	
+	//Setters:
+	public void setLeft(TreeNode x){left = x;}
+	public void setRight(TreeNode x){right = x;}
+	public void setPrev(TreeNode x){prev = x;}
+	//Getters:
+	public TreeNode getLeft(TreeNode x){return left;}
+	public TreeNode getRight(TreeNode x){return right;}
+	public TreeNode getPrev(TreeNode x){return prev;}
 	
 	
 	
