bdaa9cd0948a115deb108a87191eb5dd7da04bc7
Ryan Fleck <ryan.fleck@protonmail.com>
2017-05-23 21:14:00 -0400

    Updated sync.sh for more useful commit messages during scripted sync.

--- sync.sh
+++ sync.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-echo "Synchronizing git repo."
+echo "Synchronizing git repo. Commit message: '$1':."
+
+if [[ -z $1 ]]; 
+then
+	echo "No input. Using default commit message."
+	MSG="Automatic bash-script commit."
+else
+	MSG=$1
+fi
 
 git pull
 git add .
 git status
-git commit -m "Scripted sync."
+git commit -m "$MSG"
 git push
