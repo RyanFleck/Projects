6b4dfdd27763a82676b5ace5a50f648c24e670cb
Ryan Fleck <ryan.fleck@protonmail.com>
2017-05-26 20:47:39 -0400

    Automatic bash-script commit.

--- Linux/setupOS.sh
+++ Linux/setupOS.sh
@@ -54,7 +54,7 @@ git config --global user.name "Ryan Fleck"
 git config --global user.email "ryan.fleck@protonmail.com"
 git config --global core.editor vim
 git config --global credential.helper 'cache --timeout=7200'
-echo"Saved git settings:"
+echo "Saved git settings:"
 git config --list
 
 
