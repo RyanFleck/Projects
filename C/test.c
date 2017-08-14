2e6a289f04bacb2ad4472b84d706770a8d7bdbd1
Ryan Fleck <ryan.fleck@protonmail.com>
2017-08-13 22:22:07 -0400

    Wrote testAnnouncer function for new C learning program.

--- C/test.c
+++ C/test.c
@@ -6,8 +6,21 @@
  * Kernighan/Ritchie 2ed
  * Ryan Fleck's Spaghetti File. */
 
+int announceTest();
 //Functions based on problems in the textbook:
 
+
 int main(){
-  printf("C program 'test.c' compiled correctly.");
+  printf("C program 'test.c' compiled correctly.\nRunning test functions:\n");
+  
+  //CHAPTER ONE
+
+  return 0;
 }
+int announceTest(char testName[]){
+  printf( "\nFunction '%s' output:\n\n",testName );
+  return 0;
+}
+
+//CHAPTER ONE FUNCTIONS
+
