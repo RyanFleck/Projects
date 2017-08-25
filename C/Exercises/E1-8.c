94c974b96044508934d7647c72002eb457ea3f88
Ryan Fleck <ryan.fleck@protonmail.com>
2017-08-25 12:09:43 -0400

    Automatic bash-script commit.

--- C/Exercises/E1-8.c
+++ C/Exercises/E1-8.c
@@ -10,18 +10,20 @@ int main(void){
 	printf("RCF.TestProg.%d-%d.START\n\n",CHAPTER,EXERCISE);
 	//Beginning of exercise code: Program counts blanks, tabs and newlines for a given input file.
 
-  int c,
-      bl, //Blanks
-      tb, //Tabs
-      nl; //Newlines
+  int c=0,
+      bl=0, //Blanks
+      tb=0, //Tabs
+      nl=0; //Newlines
 
   while( (c=getchar()) != EOF ){
-    if(c==' '){++bl;}
-    if(c=='\t'){++tb;} 
-    if(c=='\n'){++nl;}
+    if(c==' '){++bl;  printf("Processed: Blank.\tTotal Blanks: %i\n",bl);} 
+    if(c=='\t'){++tb; printf("Processed: Tab. \tTotal Tabs: %i\n",tb);} 
+    if(c=='\n'){++nl; printf("Processed: Newline. \tTotal Newlines: %i\n",nl);}
     }
 
-  printf("Blanks:%i\nTabs:%i\nNewlines:%i\n",bl,tb,nl);
+  printf("\nItems in file:\nBlanks:%i\nTabs:%i\nNewlines:%i\n",bl,tb,nl);
+
+  //I apologize for the verbosity, but I had an error where 'nl' would start counting from 3700.
 
 	//End of exersise code.
 	printf("\nRCF.TestProg.%d-%d.END\n",CHAPTER,EXERCISE);
