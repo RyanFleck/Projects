1c7e636b5457c3494fd36d8b2c79b96d3e4329bc
Ryan Fleck <ryan.fleck@protonmail.com>
2017-08-25 10:23:45 -0400

    Added exercises folder and boilerplate C code for exercises.

---
+++ C/Exercises/boilerplate.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+//Ryan Fleck - Learning C - Practice Exercise.
+//From "The C Programming Language" 2e, K&R
+
+#define CHAPTER  0		
+#define EXERCISE 0
+
+void main(){
+	printf("RCF.TestProg.%d-%d.START\n\n",CHAPTER,EXERCISE);
+	//Beginning of exercise code:
+
+
+
+
+
+
+
+
+
+	//End of exersise code.
+	printf("\n\nRCF.TestProg.%d-%d.END\n",CHAPTER,EXERCISE);
+}
+
