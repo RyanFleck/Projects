e00b6b40d409f80d152809e2baebc24de431450b
Ryan Fleck <ryan.fleck@protonmail.com>
2017-08-31 12:20:54 -0400

    Scripted commit.

---
+++ C/Exercises/r.pointertest.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+//Ryan Fleck - Learning C - Practice Exercise.
+//From "The C Programming Language" 2e, K&R
+//PROGRAM DESC:
+//
+
+#define CHAPTER  00		
+#define EXERCISE 00
+
+int main(void){
+	printf("RCF.TestProg.%d-%d.START\n\n",CHAPTER,EXERCISE);
+	//Beginning of exercise code:
+
+
+
+
+
+
+
+
+
+	//End of exersise code.
+	printf("\n\nRCF.TestProg.%d-%d.END\n",CHAPTER,EXERCISE);
+	return 0;
+}
+
