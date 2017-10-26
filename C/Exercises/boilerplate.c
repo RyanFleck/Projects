8484233a40d4e51c64176ba291415b0f074b8820
Ryan Fleck <ryan.fleck@protonmail.com>
2017-10-26 10:04:17 -0400

    Finalized new boilerplate

---
+++ C/Exercises/boilerplate.c
@@ -0,0 +1,34 @@
+/*
+* ----------------------------------------------------------------------------
+* "THE BEER-WARE LICENSE" (Revision 42-R):
+* <rflec028@uottawa.ca> wrote this file. As long as you retain this notice you
+* can do whatever you want with this stuff. If we meet some day, and you think
+* this stuff is worth it, you can buy me a beer in return  -Ryan Fleck
+* ----------------------------------------------------------------------------
+*
+* Ryan Fleck - Learning C - Textbook Exercise
+* BEERWARE license written by Poul-Henning Kamp <phk@FreeBSD.ORG> 
+* Exercises from "THE C PROGRAMMING LANGUAGE" 2E, K&R
+*/
+
+#include<stdio.h>
+#define CHAPTER  00		
+#define EXERCISE 00
+
+int main(void){
+	printf("RCF.TestProg.%d-%d.START\n\n",CHAPTER,EXERCISE);
+	//Beginning of exercise code:
+
+
+
+
+
+
+
+
+
+	//End of exersise code.
+	printf("\n\nRCF.TestProg.%d-%d.END\n",CHAPTER,EXERCISE);
+	return 0;
+}
+
