b42ee50bb0faa1f4f64ceabccbe6c60a43868cfb
Ryan Fleck <ryan.fleck@protonmail.com>
2017-11-02 06:25:08 -0400

    Updated REMOTELAB ip address.

--- C/Exercises/C01E13.c
+++ C/Exercises/C01E13.c
@@ -9,24 +9,39 @@
 * Ryan Fleck - Learning C - Textbook Exercise
 * BEERWARE license written by Poul-Henning Kamp <phk@FreeBSD.ORG> 
 * Exercises from "THE C PROGRAMMING LANGUAGE" 2E, K&R
+*
+* Prints a histogram of the lengths of words within the input.
 */
 
 #include<stdio.h>
 #define CHAPTER  1		
-#define EXERCISE 12
+#define EXERCISE 13
+#define MAXLEN 10
 
 int main(void){
 	printf("RCF.TestProg.%d-%d.START\n\n",CHAPTER,EXERCISE);
 	// Beginning of exercise code.
 
-  int c;
+  int c,i,whitespace,large;
+  int wordlen[MAXLEN];
 
-  while(  (c=getchar()) != EOF  ){
-    if( c == ' ' || c == '\n' || c == '\t')
-      putchar('\n');
-    else
-      putchar(c);
+  // Instantiaion:
+  c = i = whitespace = large = 0;
+  for ( i ; i<MAXLEN ; ++i ){
+    wordlen[i]=0;
+    printf("Indice %d is length %d.\n",i,wordlen[i]);
     }
+
+  // Text processor:
+  while(  (c=getchar()) != EOF  ){
+    
+    //Ensure word is less than maximum length: 
+    if( (i<9)&&(c)&&(c)&&(c) ){
+       
+      }
+
+    }
+
 
 	// End of exersise code.
 	printf("\n\nRCF.TestProg.%d-%d.END\n",CHAPTER,EXERCISE);
