0ef3d4dc0eb7de55ab5fe5c4b4e0bb86a35c83f9
Ryan Fleck <ryan.fleck@protonmail.com>
2017-11-29 21:25:08 -0500

    Improved remotelab, writes to home directory.

--- Linux/ssh/rcf-remotelab.connect.sh
+++ Linux/ssh/rcf-remotelab.connect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ssh -XC rflec028@142.116.173.49
-#RCF - Last updated Wed Nov 29 21:08:21 EST 2017
+#RCF - Last updated Wed Nov 29 21:25:00 EST 2017
--- Linux/ssh/updateip.sh
+++ Linux/ssh/updateip.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "Running..."
 ip=`wget -qO- http://ipecho.net/plain`
 name=`hostname`
 name=${name,,}
@@ -9,3 +8,7 @@ echo "#!/bin/bash" > $prog
 echo "ssh -XC rflec028@$ip" >> $prog
 echo "#RCF - Last updated $date" >> $prog
 chmod +x $prog
+for x in *.connect.sh; do
+  \cp $x ~/$x
+done
+
