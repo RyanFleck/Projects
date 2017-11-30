9408b2e5b0149f3798f1eb8baf4ad3ad5c9ee085
Ryan Fleck <ryan.fleck@protonmail.com>
2017-11-29 21:50:46 -0500

    Enabled automatic github update so all machines can have current IPs.

--- Linux/ssh/updateip.sh
+++ Linux/ssh/updateip.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+
+#GATHER INFORMATION
 ip=`wget -qO- http://ipecho.net/plain`
 name=`hostname`
 name=${name,,}
 date=`date`
 prog=$name.connect.sh
+data=ip.$name
+lastip=`cat $data`
+
+#WRITE FILES
+echo "$ip" > $data
 echo "#!/bin/bash" > $prog
 echo "ssh -XC rflec028@$ip" >> $prog
 echo "#RCF - Last updated $date" >> $prog
@@ -12,4 +19,12 @@ mkdir -p ~/Remote
 for x in *.connect.sh; do
   \cp $x ~/Remote/$x
 done
+
+#UPLOAD CHANGES
+if [ $lastip != $ip ]; then
+  echo "IP Changed from $lastip to $ip! Pushing changes."
+  git add $prog
+  git commit -m "RCF-AUTO: Updated IP for $name."
+  git push
+fi
 
