19d9f8a2a91f2a3d481319280babbdbfc62ce63a
Ryan Fleck <ryan.fleck@protonmail.com>
2018-01-03 11:48:18 -0700

    Finished shader compiler. Troubleshooting.

--- HTML5_JS/RCF-G01/RCF-G01/public_html/js/main.js
+++ HTML5_JS/RCF-G01/RCF-G01/public_html/js/main.js
@@ -1,4 +1,4 @@
-/* global gl */
+/* global gl, gSimpleShader, gShaderVertexPositionAttribute */
 
 "use strict";
 var gl = null;
@@ -9,6 +9,8 @@ gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
 
 if(gl !== null){
     gl.clearColor(0.2,0.2,0.2,1.0);
+    initSquareBuffer();
+    initSimpleShader("VertexShader","FragmentShader");
 }else{
     document.write("<br><b>No WebGL support!</b>");
 }
@@ -21,4 +23,11 @@ function clearCanvas(){
 function doDraw(){
     initializeGL();
     clearCanvas();
+    drawSquare();
+}
+
+function drawSquare(){
+    gl.useProgram(gSimpleShader);
+    gl.enableVertexAttribArray(gShaderVertexPositionAttribute);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 }
