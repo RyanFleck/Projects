629ff1589342d0400fdb26897fddae407decf473
Ryan Fleck <ryan.fleck@protonmail.com>
2018-05-02 21:29:24 -0400

    Added to LISP materials.

--- LISP/prog1.lisp
+++ LISP/prog1.lisp
@@ -1,8 +1,19 @@
 (defparameter *big* 100)
-(defparameter *small* 0)
+(defparameter *small* 1)
 
 (defun guess-my-number ()
   (ash (+ *small* *big*) -1)
-
-
+)
+(defun smaller ()
+  (setf *big* (1+ (guess-my-number)))
+  (guess-my-number)
+)
+(defun bigger () 
+  (setf *small* (1+ (guess-my-number)))
+  (guess-my-number)
+)
+(defun start-over ()
+  (defparameter *small* 1)
+  (defparameter *big* 100)
+  (guess-my-number)
 )
