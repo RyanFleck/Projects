dcec834951e706a8c28d2387604f0f4e2ff550c7
Ryan Fleck <ryan.fleck@protonmail.com>
2018-05-14 16:57:32 -0400

    Added unique methods to the classes in the namespace to test importing/calling them.

--- cs/RCF003_Namespaces.cs
+++ cs/RCF003_Namespaces.cs
@@ -2,21 +2,26 @@ using System;
 
 namespace RCF_NSP01
 {
-
-class RCF_CLS01 //Classes can be loaded separately by other programs! Ex. `using Namespace.Class;`
-{
-  static void Main()
+  class RCF_CLS01 //Classes can be loaded separately by other programs! Ex. `using Namespace.Class;`
   {
-    Console.WriteLine("Class one!");
+    static void Main()
+    {
+      Console.WriteLine("Class one!");
+    }
+    static int Double(int x)
+    {
+      return x*2;
+    }
   }
-}
-
-class RCF_CLS02
-{
-  static void Main()
+  class RCF_CLS02
   {
-    Console.WriteLine("Class two!");
+    static void Main()
+    {
+      Console.WriteLine("Class two!");
+    }
+    static int DivideByTwo(int x)
+    {
+      return x/2;
+    }
   }
-}
-
 }
