ba3aedb76708e828625fbac7e5214ebd7896e4f4
Ryan Fleck <ryan.fleck@protonmail.com>
2018-05-14 22:52:43 -0400

    Tinkering with C#: Added Types.cs

--- cs/RCF004_Syntax.cs
+++ cs/RCF004_Syntax.cs
@@ -5,6 +5,8 @@ class Program
   static void Main()
   {
     int a = 123/4;
+    int @int = 3;
     Console.WriteLine(a);
+    Console.WriteLine(@int);
   }
 }
---
+++ cs/RCF005_Types.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+  static void Main()
+  {
+    int x = 144/12;
+    string message = "Hello World!";
+    const int life = 42;
+    bool george = true;
+    Console.WriteLine(x + message + life + george);
+
+  }
+}
