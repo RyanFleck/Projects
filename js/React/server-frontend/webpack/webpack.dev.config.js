9d644391a43816fac4269cb875ec0fa2f56b5c09
Ryan Fleck <ryan.fleck@protonmail.com>
2018-07-07 08:11:40 -0400

    Update resume, add webpack config.

---
+++ js/React/server-frontend/webpack/webpack.dev.config.js
@@ -0,0 +1,21 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	entry: [
+		path.join(__dirname, '../index.js')
+	],
+	module: {
+		loaders: [{
+			test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},{
+				test: /\.less$/,
+				loaders: ["style-loader", "css-loader", "less-loader"]
+			}
+		]
+	},
+}
+view raw
+
