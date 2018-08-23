cc9b43cefbe14cb8a97996214999b3fca751a307
Ryan Fleck <ryan.fleck@protonmail.com>
2018-08-23 19:55:24 -0400

    Add some algos.

---
+++ js/rcf018_SumSqDiff.js
@@ -0,0 +1,26 @@
+// Calculate the difference between the sum of squares and square of sums.
+
+alg6(10);
+alg6(100);
+
+function alg6( x ){
+  console.log(x);
+  let answer = squareOfSum(x) - sumOfSquares(x); 
+  console.log(answer);
+
+  function sumOfSquares(y){
+    let acc = 0;
+    for(let z = 1; z <= y; z++){
+      acc+=(z*z);
+    }
+    return acc;
+  }
+  
+  function squareOfSum(y){
+    let acc = 0;
+    for(let z = 1; z <= y; z++){
+      acc+=z; 
+    }
+    return acc*acc ;
+  }
+}
