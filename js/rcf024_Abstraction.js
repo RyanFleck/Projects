703f747182e3b6fdaf1763da8d23a697ca17b297
Ryan Fleck <ryan.fleck@protonmail.com>
2018-08-25 06:04:08 -0400

    Add useful functions to `rcf_lib.js`

---
+++ js/rcf024_Abstraction.js
@@ -0,0 +1,8 @@
+const r = require('./rcf_lib');
+
+r.title("Testing");
+
+console.log(r.range(12));
+
+r.sum(12);
+r.sum("Hello");
---
+++ js/rcf_lib.js
@@ -0,0 +1,74 @@
+// Useful functions for handling prime numbers.
+// Just temp until I can get home and install math.js module.
+
+// Returns true if x is prime. 
+function primep( x ){
+  let sqx = parseInt(Math.sqrt(x)); 
+  for(let i = 2; i <= sqx; i++){
+    if ( x % i == 0 )
+      return false;
+  }
+  return true
+}
+
+// Returns true if x is a factor of y.
+function factorp(x,y){
+  return ((y % x) == 0);
+}
+
+// Sums primes from two to x.
+function sumto(limit){
+  let sum = 2
+
+  for(let x = 3; x < limit; x+=2){
+    if(primep(x))
+      sum+=x;
+  }
+  console.log("Primes below "+limit+" add to "+sum);
+  return sum;
+}
+
+// Returns array with numbers 1 through x
+function range(x){
+  const arr = [];
+  for(let y = 0; y<x; y++){
+    arr.push(y);
+  }
+  return arr;
+}
+
+//Returns sum based on type.
+function sum(x){
+  let arglen = arguments.length;
+  if(arglen > 1){
+    //Ensure all args are same type.
+    //Sum args.
+    return 0;
+  }else if(arglen==1){
+    switch(typeof x){
+      default:
+        console.log(typeof x);
+        break;
+    } 
+  }else{
+    return 0; 
+  }
+ console.log("Sum "+arguments.length+" args: "+arguments.toString());
+}
+
+function title( y ){
+  console.log(ul(y));
+}
+
+// Internal Library Methods
+
+function ul(str){
+  return("\n\n\t"+str+"\n\t"+"-".repeat(str.length)+"\n");
+}
+
+
+function prObj(name,obj){
+console.log("\n"+name+" Properties:\n"+JSON.stringify(obj,null,2));
+}
+
+module.exports = { title, primep, factorp, sumto, range, sum };
