0d690a90e2d0d683aacc30808ad05f7a2e42653c
Ryan Fleck <ryan.fleck@protonmail.com>
2018-08-27 20:10:43 -0400

    Successfully solved grid problem.

--- js/rcf030_CorkJS/src/Note.js
+++ js/rcf030_CorkJS/src/Note.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 // react-icons refused to load, was issue with NPM.
-import FaPencil from 'react-icons/lib/fa/pencil'
-import FaTrash from 'react-icons/lib/fa/trash'
+import { FaPencil } from 'react-icons/fa'
+import { FaTrash } from 'react-icons/fa'
 
 
 class Note extends Component {
