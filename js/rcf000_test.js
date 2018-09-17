baaa89fe652c81b415824a566f4da3c75c40b271
Ryan Fleck <ryan.fleck@protonmail.com>
2018-09-17 14:28:05 -0400

    Junk

--- js/rcf000_test.js
+++ js/rcf000_test.js
@@ -1,3 +1,13 @@
 // This file is a placeholder until I write proper tests for this repo.
 
 console.log('TEST: Hello, World!')
+
+function increment( y ){
+  y = y+1
+  return y
+}
+
+let x = 1
+
+console.log(increment(x))
+console.log(x)
--- scala/spark-shell.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Attempting to open spark-shell."
-cd ~/Documents/spark/bin/
-./spark-shell
