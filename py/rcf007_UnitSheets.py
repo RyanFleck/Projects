9ae6c5fae8c77ed258ea23aec8b67c58acfa62bb
Ryan Fleck <ryan.fleck@protonmail.com>
2018-09-21 12:07:59 -0400

    Commit current WIP excel manipulation tools.

---
+++ py/rcf007_UnitSheets.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# Dynamically generates unittests based on table data.
+
+import random
+import unittest
+#import selenium
+#from selenium import webdriver
+#from selenium.webdriver.support.ui import Select
+#from selenium.webdriver.chrome.options import Options
+import time
+import sys
+from rcf_lib import TableGlob
+from rcf_logs import dbg
+
+# Program Information and Options
+revision = "1.1.1"
+debug = False #True enables verbose logging {and Chrome GUI}.
+
+def iterate(x):
+  x = x+1
+  return x
+
+
+
+
+class TestMethods(unittest.TestCase):
+
+  def test_iterate(self):
+    self.assertTrue( 1 == 1 )
+
+if __name__ == '__main__':
+  unittest.main()
--- py/rcf_lib.py
+++ py/rcf_lib.py
@@ -9,12 +9,18 @@ import os
 import pandas as pd
 import xlrd
 
-# True for debugging:
-e = True
+e = False #Debug.
 
 def title( title_string ):
     print("\n\t "+title_string)
     print("\t+"+(len(title_string)*"-")+"+\n")
+
+def subtitle( subtitle_string ):
+    print(" "+subtitle_string)
+    print("-"+(len(subtitle_string)*"-")+"-+\n")
+
+
+# Excel Spreadsheet I/O
 
 class TableGlob:
 
@@ -56,30 +62,55 @@ class TableGlob:
         dbg(e,"good","TblGlb","Data Shape (ROW,COL): {0}".format(tuples))
         return tuples
 
+    # Exports to the same directory as the imported file with a mutated
+    # filename. Mutation in the form string, "{0}-out" , where {0} inserts
+    # the original filename. ".xslx" is automatically appended.
+    def exportExcelTable(self, mutation):
+        return 0
+
+# Word Document I/O
+
+class DocuGlob:
+
+    def __init__(self, name):
+        self.name = name
+
+    def importWordDocument(self):
+        return 0
+
+    def exportWordDocument(self):
+        return 0
+
+    # TODO: Implement me :)
 
 
-
+# Unit Tests for RCF-Lib methods.
 
 class TestMethods(unittest.TestCase):
 
   def test_title(self):
-      dbg(e,"good","Unit","Testing title.")
-      print("This should display as a title:")
+      dbg(True,"good","Unit","Testing title.")
+      print("A title and subtitle should be visible below:")
       title("Wow, Look, a Title!")
+      subtitle("Wow, Look, a SubTitle!")
 
 class TestTableGlob(unittest.TestCase):
 
+    # Change to match current test table.
+    testTablePath = "tables/TestTable.xlsx"
+
     def test_01_import(self):
-        dbg(e,"good","Unit","Testing table import.")
+        dbg(True,"good","Unit","Testing table import.")
         t = TableGlob("Primary Table")
-        self.assertTrue(t.importExcelTable("tables/TestTable.xlsx"))
-        self.assertFalse(t.importExcelTable("tables/TestTable.xlsx"))
+        self.assertTrue(t.importExcelTable(self.testTablePath))
+        self.assertFalse(t.importExcelTable(self.testTablePath))
 
     def test_02_dataframe(self):
-        dbg(e,"good","Unit","Testing table import: data shape tuples.")
+        dbg(True,"good","Unit","Testing table import: data shape tuples.")
         t = TableGlob("Primary Table")
-        self.assertTrue(t.importExcelTable("tables/TestTable.xlsx"))
+        self.assertTrue(t.importExcelTable(self.testTablePath))
         self.assertIsNotNone(t.dataShapeTuple())
 
 if __name__ == '__main__':
-  unittest.main()
+    e=True #Debug.
+    unittest.main()
