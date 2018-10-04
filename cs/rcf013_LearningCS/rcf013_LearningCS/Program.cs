8d5c2310b89afd64073d55d9a480187c8c95966a
Ryan Fleck <ryan.fleck@protonmail.com>
2018-10-04 17:59:04 -0400

    Add new C# project for continued Sitecore learning.

---
+++ cs/rcf013_LearningCS/rcf013_LearningCS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace rcf013_LearningCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Initial project size only 256k. That's alright.
+             */
+
+
+
+            // Keep console open at end of program.
+            Console.WriteLine("FIN. Press ENTER.");
+            Console.ReadKey();
+        }
+    }
+}
