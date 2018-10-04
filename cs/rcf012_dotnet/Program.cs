05591f564e396239fb2ac0f1e5c4a0934ba7c064
Ryan Fleck <ryan.fleck@protonmail.com>
2018-10-04 17:40:04 -0400

    Touch a dotnet project in VS code...

--- cs/rcf012_dotnet/Program.cs
+++ cs/rcf012_dotnet/Program.cs
@@ -7,6 +7,8 @@ namespace rcf012_dotnet
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.ReadKey(); // Prevent immediate closure at end of program.
         }
     }
 }
