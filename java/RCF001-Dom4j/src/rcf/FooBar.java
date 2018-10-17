a32e8ebe2f5445a55e8a0f2fe663b9f3d83daf24
Ryan Fleck <ryan.fleck@protonmail.com>
2018-10-17 15:24:59 -0400

    Add javadoc and printElementInfo method.

--- java/RCF001-Dom4j/src/rcf/FooBar.java
+++ java/RCF001-Dom4j/src/rcf/FooBar.java
@@ -6,6 +6,7 @@
  */
 package rcf;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import org.dom4j.Document;
 import org.dom4j.Element;
@@ -34,6 +35,8 @@ public class FooBar {
     public static void main(String[] args) {
         System.out.println("Dom4j XML-Processing Experiment.");
         
+        // 1 - Parsing XML from a file.
+        
         // Initialize SAXReader built into Dom4j:
         SAXReader reader = new SAXReader();
         Document doc = null;
@@ -44,6 +47,8 @@ public class FooBar {
             System.out.println("Document read error.\n"+de.toString());
             System.exit(0);
         }
+        
+        // 2 - Iterating through elements in a document.
         
         // Iterate through the document.
         Element root = doc.getRootElement();
@@ -71,6 +76,10 @@ public class FooBar {
          *       guid: http://octranspo1.com/update-d...
          */
         
+        // 3 - Applying XPath expressions to select nodes.
+        
+        printElementInfo(root);
+        
         
     }
     
@@ -78,10 +87,13 @@ public class FooBar {
      * @param documentRoot Root Element of XML document as read by a SAXReader.
      */
     public static void recursiveWalkDOM(Element documentRoot ) {
-        System.out.println("Recursively walk DOM for "+documentRoot.getName()+", printing out tags.");
+        System.out.println("\nRecursively walk DOM for "+documentRoot.getName()+", printing out tags:\n");
         recursiveStep(documentRoot,0);
     }
-    
+
+    /**
+     * Recursively iterates through XML elements.
+     */
     private static void recursiveStep(Element x, int depth) {
         System.out.println(rep(depth,"  ")+x.getName()+": "+rTrimString(x.getTextTrim(),30));
         for (Iterator<Element> it = x.elementIterator(); it.hasNext();) {
@@ -90,6 +102,11 @@ public class FooBar {
         }
     }
     
+    /**
+     * Repeats a string a specified number of times.
+     * @param c Number of repititions.
+     * @param s String to repeat.
+     */
     private static String rep(int c, String s) {
         StringBuilder repeated = new StringBuilder(c);
         for(int i=0; i<c; i++) {
@@ -97,13 +114,44 @@ public class FooBar {
         }
         return repeated.toString();
     }
-    
+
+    /**
+     * If a string is over a character length, trim and add an ellipsis.
+     */
     private static String rTrimString( String s, int length) {
         if(s.length() > length) {
-            return s.substring(0,length).concat("...");
+            return s.substring(0,(length-3)).concat("...");
         } else {
             return s;
         }
+    }
+    
+    /**
+     * Prints the name, children array and short content of an XML node.
+     */
+    private static void printElementInfo(Element x) {
+        
+        String name;
+        String content;
+        String children;
+        
+        ArrayList<String> childArray = new ArrayList<>();
+        
+        name = x.getName();
+        
+        content = rTrimString(x.getTextTrim(),40);
+        if(content.equals("")) {
+            content = "No content.";
+        }
+        
+        for (Iterator<Element> it = x.elementIterator(); it.hasNext();) {
+            Element element = it.next();
+            childArray.add(element.getName());
+        }
+        
+        children = childArray.toString();
+        
+        System.out.println("\nName: "+name+"\nContent: "+content+"\nChildren: "+children+"\n");
     }
 
 
