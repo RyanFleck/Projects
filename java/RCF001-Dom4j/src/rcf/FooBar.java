a16f7ab548c78c1a4f4dbe840743739cfd8c40b8
Ryan Fleck <ryan.fleck@protonmail.com>
2018-10-17 11:52:30 -0400

    Add Dom4j experiment Java Project

---
+++ java/RCF001-Dom4j/src/rcf/FooBar.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package rcf;
+
+import java.net.URL;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
+
+/**
+ * 
+ */
+public class FooBar {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Dom4j XML-Processing Experiment.");
+
+    }
+
+}
