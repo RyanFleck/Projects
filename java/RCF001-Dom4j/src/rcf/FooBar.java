8fc413dc02ee2dcac971ab542e8fb938bb39422d
Ryan Fleck <ryan.fleck@protonmail.com>
2018-10-17 12:10:22 -0400

    Dom4j basic document import successful.

--- java/RCF001-Dom4j/src/rcf/FooBar.java
+++ java/RCF001-Dom4j/src/rcf/FooBar.java
@@ -1,12 +1,16 @@
 /**
+ * @author Ryan.Fleck
+ * @version 1.0.0 2018-10-17
  * 
+ * FooBar is a test class for testing the Dom4j library.
  */
 package rcf;
 
-import java.net.URL;
 import org.dom4j.Document;
+import org.dom4j.Element;
 import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
+
 
 /**
  * 
@@ -14,12 +18,31 @@ import org.dom4j.io.SAXReader;
 public class FooBar {
 
     /**
-     * @param args
+     * This PSVM runs through the following concepts from the Dom4j page:
+     *  - Parsing XML from a file.
+     *  - Iterating through elements in a document.
+     *  - Applying XPath expressions to select nodes.
+     *  - Fast looping method for large documents.
+     *  - Creating/writing XML documents to file system.
+     * 
+     * @param args Canonical input-stream processing variable.
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         System.out.println("Dom4j XML-Processing Experiment.");
-
+        
+        // Initialize SAXReader built into Dom4j:
+        SAXReader r = new SAXReader();
+        Document d = null;
+        try {
+            d = r.read("data/oct-en.xml");
+            System.out.println("Document read to var 'd'.");
+        } catch (DocumentException e) {
+            System.out.println("Document read error.\n"+e.toString());
+            System.exit(0);
+        }
+        
+        // Inspect the document and pull data?
+        Element docRoot = d.getRootElement();
     }
 
 }
