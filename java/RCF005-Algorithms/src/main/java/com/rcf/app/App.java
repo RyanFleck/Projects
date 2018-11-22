b39a51677bcbbd61f32d0eb5a30b4aeb86159178
Ryan Fleck <ryan.fleck@protonmail.com>
2018-11-22 10:24:02 -0500

    Fiddle with jUnit.

--- java/RCF005-Algorithms/src/main/java/com/rcf/app/App.java
+++ java/RCF005-Algorithms/src/main/java/com/rcf/app/App.java
@@ -4,10 +4,11 @@ package com.rcf.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("");
+	}
+	public static boolean jUnitTest() {
+		return true;
+	}
 }
--- java/RCF005-Algorithms/src/test/java/com/rcf/app/AppTest.java
+++ java/RCF005-Algorithms/src/test/java/com/rcf/app/AppTest.java
@@ -7,32 +7,40 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Rigourous Test :-)
+	 */
+	@Test
+	public void jUnitFunctional() throws Exception{
+		System.out.println("1 Running test...");
+		assertTrue( true );
+	}
+	
+	public void testJUnitMethod() throws Exception{
+		System.out.println("2 Running test...");
+		assertTrue(App.jUnitTest());
+	}
+	
+	public void thisShouldFail() throws Exception{
+		System.out.println("3 Running test...");
+		assertTrue( false );
+	}
 }
