7dd395cc39f6463421074d237f9cacd3edc05030
Ryan Fleck <ryan.fleck@protonmail.com>
2018-12-08 21:24:15 -0500

    Teeny c repl

---
+++ c/rcf010_repl3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+static char input[2048];
+
+int main( int argc, char** argv ){
+    puts("TestREPL v1. Depress CTRL+C to exit.");
+    while(1){
+        fputs("\n >  ", stdout);
+        fgets(input, 2048, stdin);
+        printf(" => %c", input[0]);
+    }
+    return 0;
+}
