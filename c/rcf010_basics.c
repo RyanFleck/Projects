dd927b63f58a571465029b3e715334e4fa5c6dc3
Ryan Fleck <ryan.fleck@protonmail.com>
2018-12-08 21:11:27 -0500

    Tinker with c/lisp.

--- c/rcf010_basics.c
+++ c/rcf010_basics.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
     float x;
@@ -7,10 +8,17 @@ typedef struct {
 } vec;
 
 vec addVec();
+int printVec();
 int helloLoop();
 
 int main(int argc, char** argv) {
     puts("Hello, World!");
+
+    if(secure_getenv("RCF_IS_DEBIAN")){
+        printf("RCF_IS_DEBIAN environment variable is set to %s.\n",getenv("RCF_IS_DEBIAN"));
+    }else{
+        puts("RCF_IS_DEBIAN is false or not present.");
+    }
 
     vec a;
     a.x = 1;
@@ -68,11 +76,11 @@ int main(int argc, char** argv) {
      */
 
     int x, y, z;
-    float g, h, i;  // 4 bytes, 6 digit precision.
-    double j, k, l; // 8 Bytes, 14 digit precision.
+    float fg, fh, fi;  // 4 bytes, 6 digit precision.
+    double dj, dk, dl; // 8 Bytes, 14 digit precision.
 
     long double m, n; // long can increase the size of numbers.  
-    short float l, o; // short can decrease the size of numbers. 
+    short l, o; // short can decrease the size of numbers. 
     unsigned int p; // Can only hold positive numbers, but x^2 more.
     const int r; // Cannot be changed later.
     
@@ -80,28 +88,50 @@ int main(int argc, char** argv) {
 
     // What other conditional operators are there other than >, < ?
 
-
     // What other mathematical operators are there other than +, - ?
 
-
     // What is the += operator, and how does it work?
-
+        // Increments a variable by the value presented on the right.
 
     // What is the do loop, and how does it work?
+        // A while loop that ensures a condition at the end.
+        // Is guaranteed to execute at least once.
 
+    int iz = 0;
+    do {
+        puts("Iterate.");
+        iz += 3;
+    } while ( iz < 2 );
 
     // What is the switch statement and how does it work?
+        // Based on a case, will execute an action.
 
+    char zone = 'C';
+
+    switch(zone){
+        case 'A' :
+            puts("Zone A triggered.");
+            break;
+        case 'B' :
+            puts("Zone B triggered, deploying additional security.");
+            break;
+        case 'C' :
+            puts("Zone C triggered, breach imminent.");
+            break;
+        default :
+            puts("No breach detected.");
+    }
 
     // What is the break keyword and what does it do?
-
+        // Terminates the switch.
+        // If no break is present, the switch will continue.
 
     // What is the continue keyword and what does it do?
-
+        // Passes directly to for/while/do-while logic.
 
     // What does the typedef keyword do exactly?
-    //  As seen above when defining vec, a 3-dimensional vector, typedef
-    //  enables the creation and renaming of builtin types.
+        // As seen above when defining vec, a 3-dimensional vector, typedef
+        // enables the creation and renaming of builtin types.
 
     return 0;
 }
@@ -116,6 +146,7 @@ vec addVec(vec a, vec b) {
 
 int printVec(vec vecA) {
     printf("Vector: %f %f %f\n", vecA.x, vecA.y, vecA.z);
+    return 0;
 }
 
 int helloLoop(int n){
@@ -123,4 +154,5 @@ int helloLoop(int n){
     for(int i=0; i < n; i++){
         printf("func\t %i Hello, World!\n",(i+1));
     }
+    return 0;
 }
