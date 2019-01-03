a7f5d9975f21d0ae5c1f9db889ffd17985f1e7f6
Ryan Fleck <ryan.fleck@protonmail.com>
2019-01-02 20:23:51 -0700

    Finish getline function for GREPlike.

--- c/rcf012_grep_ish.c
+++ c/rcf012_grep_ish.c
@@ -11,13 +11,18 @@
  *      }
  *    }
  *
+ *  Simple Run: 1. gcc *this                      -> compile.
+ *              2. clear                          -> clear screen.
+ *              3. cat tests/text.txt | ./a.out   -> pipe a file to stdin.
+ *
+ *    gcc rcf012_grep_ish.c && clear && cat tests/text.txt | ./a.out
  */
 
 #include <stdio.h>
 #define MAXLINE 1000
 
-int getline(char line[], int max);
-int strindex(char source[], char searchfor[]);
+int r_getline(char line[]);
+int r_strindex(char source[], char searchfor[]);
 
 // Search Pattern
 char pattern[] = "ould";
@@ -26,7 +31,39 @@ int main()
 {
     char line[MAXLINE];
     int  found = 0;
+    int  lineno = 0;
 
+    while( r_getline(line) > 0 ) {
+        printf("%i  %s", lineno, line);
+        lineno++;
+    }
 
     return found;
+}
+
+int r_getline(char line[])
+{
+    int c, i;
+    int bound = MAXLINE - 1;
+
+    // Alternatively, this can all be a one-liner: fgets(line, MAXLINE, stdin);
+
+    for( i=0; i < bound && (c=getchar())!=EOF && c!='\n'; i++) {
+        line[i] = c;
+    }
+
+    if( c == '\n' ) {
+        line[i] = c;
+        i++;
+    }
+
+    line[i] = '\0';
+
+    return i;
+}
+
+int r_strindex(char source[], char searchfor[])
+{
+
+
 }
