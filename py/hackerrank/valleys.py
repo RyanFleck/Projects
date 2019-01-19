eaaeb2777357fbbb82abe32641157490a9604cb5
Ryan Fleck <ryan.fleck@protonmail.com>
2019-01-19 16:29:34 -0500

    AutoPEP8 strikes.

--- py/hackerrank/valleys.py
+++ py/hackerrank/valleys.py
@@ -7,31 +7,34 @@ import re
 import sys
 
 # Complete the countingValleys function below.
+
+
 def countingValleys(n, s):
-    c = 0 # Counter
-    e = 0 # Elevation
-    vf = False # valleyflag
-    vc = 0 # valleycounter
+    c = 0  # Counter
+    e = 0  # Elevation
+    vf = False  # valleyflag
+    vc = 0  # valleycounter
 
     while c < n:
 
         # Save elevation data
-        if(s[c] == 'U'):
+        if s[c] == 'U':
             e = e + 1
-        if(s[c] == 'D'):
+        if s[c] == 'D':
             e = e - 1
 
-        if(e < 0 and vf == False):
+        if e < 0 and not vf:
             vf = True
 
-        if(e >= 0 and vf == True):
+        if e >= 0 and vf
             vc += 1
             vf = False
 
         print(str(s[c]) + ", elevation = "+str(e))
-        c+=1
+        c += 1
 
     return vc
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
