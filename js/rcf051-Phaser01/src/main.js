16d8bde5975d35c68a39816b16f8b9ee5dcc943a
Ryan Fleck <ryan.fleck@protonmail.com>
2019-03-16 19:48:33 -0400

    Add sprites to Phaser game.

--- js/rcf051-Phaser01/src/main.js
+++ js/rcf051-Phaser01/src/main.js
@@ -13,8 +13,9 @@ const config = {
         },
     },
     scene: {
-        preload: preload,
-        create: create,
+        preload,
+        create,
+        update,
     },
 
 };
@@ -24,27 +25,19 @@ const game = new Phaser.Game(config);
 function preload() {
     this.load.setBaseURL('http://labs.phaser.io');
 
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude',
+        'assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 });
 }
 
 function create() {
     this.add.image(400, 300, 'sky');
+}
 
-    const particles = this.add.particles('red');
-
-    const emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD',
-    });
-
-    const logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
+function update() {
+    // no
 }
