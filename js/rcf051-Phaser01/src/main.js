d1aee7e292403905659a6024f2fdb99ca275b8e6
Ryan Fleck <ryan.fleck@protonmail.com>
2019-03-16 21:41:07 -0400

    Add dev notes to Phaser01 demo.

--- js/rcf051-Phaser01/src/main.js
+++ js/rcf051-Phaser01/src/main.js
@@ -111,3 +111,29 @@ function update() {
         player.anims.play('stop');
     }
 }
+
+/* NOTES FOR NEXT TIME:
+
+https://phaser.io/tutorials/coding-tips-005
+http://nautil.us/issue/70/variables/how-designers-engineer-luck-into-video-games-rp
+
+- Utilize tiled tilemaps.
+- Learn to use the physics engine for collisions.
+
+this.pacman.scale.x = 1;
+this.pacman.angle = 0;
+
+if (direction === Phaser.LEFT)
+{
+    this.pacman.scale.x = -1;
+}
+else if (direction === Phaser.UP)
+{
+    this.pacman.angle = 270;
+}
+else if (direction === Phaser.DOWN)
+{
+    this.pacman.angle = 90;
+}
+
+*/
