18bfa8fa557e93d542fd49d35803d7b84a3d8beb
Ryan Fleck <ryan.fleck@protonmail.com>
2019-06-01 19:36:28 -0400

    Improve prime number algo.

--- java/RCF000-Algorithms/src/main/java/com/rcf/algorithms/PrimeFactor.java
+++ java/RCF000-Algorithms/src/main/java/com/rcf/algorithms/PrimeFactor.java
@@ -2,6 +2,8 @@ package com.rcf.algorithms;
 
 import static com.rcf.algorithms.utils.PrimeUtil.*;
 
+import java.util.ArrayList;
+
 /*
  * The prime factors of 13195 are 5, 7, 13 and 29.
  * 
@@ -12,13 +14,42 @@ public class PrimeFactor {
 
     /**
      * Counts backwards from the square root of the ceil to determine the largest
+     * prime factor. Not a good solution, need to find a proper way to build and
+     * recurse through a full tree of factors.
+     * 
+     * @param ceil
+     * @return
+     */
+    public static long largestPrimeFactor( long ceil ) {
+
+        long max = (long) Math.sqrt( ceil ) + 1;
+        long first = 0;
+
+        for ( double x = 2; x < max; x++ ) {
+            double check = ceil / x;
+            if ( wholep( check ) ) {
+                long checkl = (long) check;
+                if ( first == 0 )
+                    first = checkl;
+                if ( factorp( checkl, ceil ) && primep( checkl ) ) {
+                    return checkl;
+                }
+            }
+        }
+
+        return largestPrimeFactor( first );
+    }
+
+    /**
+     * Counts backwards from the square root of the ceil to determine the largest
      * prime factor. This first attempt is a failure, as it only processes all of
      * the whole factors of ceil. Reattempt soon.
      * 
      * @param ceil
      * @return
      */
-    public static long largestPrimeFactor( long ceil ) {
+    @Deprecated
+    public static long largestPrimeFactorV1( long ceil ) {
 
         long max = (long) Math.sqrt( ceil ) + 1;
 
--- java/RCF000-Algorithms/src/main/java/com/rcf/algorithms/utils/solutions/Euler.java
+++ java/RCF000-Algorithms/src/main/java/com/rcf/algorithms/utils/solutions/Euler.java
@@ -3,11 +3,12 @@ package com.rcf.algorithms.utils.solutions;
 /**
  * Spoiler Alert! These are the final solutions for Project Euler problems. The
  * solutions to more difficult problems, along with the answers, are not listed
- * in this repository.
+ * in this repository. Generally, if your solution continues to run for more
+ * than a short while, you've done it wrong.
  */
 
 public class Euler {
     public static final int p001 = 233168;
     public static final int p002 = 4613732;
-    public static final long p003 = 0;
+    public static final long p003 = 6857;
 }
