a6c76ba6892c87a741e0bd33c82eafa4ddedd157
Ryan Fleck <ryan.fleck@protonmail.com>
2019-06-29 13:39:57 -0400

    Read until 'hashes'

---
+++ rb/whys_poignant_guide/README.md
@@ -0,0 +1,5 @@
+# Why's Poignant Guide
+
+*Oh, neat!* You've stumbled upon my notes on *Why the Lucky Stiff's (Poignant) Guide to Ruby.* It's free to read online [here](https://poignant.guide).
+
+I've split my notes/functions into chapters for now, but they may get broken down into chapter-section in the future. To get started with ruby, I've installed `rbenv` and Ruby 2.6.3.
--- rb/whys_poignant_guide/part_1_2_3.rb
+++ rb/whys_poignant_guide/part_1_2_3.rb
@@ -1,3 +1,48 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
+# Er, so I have a reason to learn Ruby now. So I figured I'd
+# take a quick read through Why's Poignant Guide and see what
+# was up with Ruby which, supposedly, is designed with human
+# happiness in mind. Worth a shot. Good to have another tool
+# on my toolbelt.
+
+# Canonical first test.
 puts 'Hello, world!'
+# Numbers have methods? What is this? (Anything in {} is a block.)
+3.times { puts 'Odelay!' }
+
+# Rubocop autocorrects the array of words here... neat.
+%w[toast hummus celery tomato].each { |food| puts 'eat some ' + food }
+
+# Variables (Use underscores to make big numbers easy to read.)
+banana_phone = 3
+use_some_underscores = 4_000_000
+no_other_punctuation = 3.14
+# Globals start with $, symbols start with :, instance variables use @,
+# class vars use @@. Phew!
+
+puts banana_phone + use_some_underscores + no_other_punctuation
+
+hello_world = 'Hello, World!'
+puts "'#{hello_world}' is #{hello_world.length} chars long."
+
+# Ranges (x..y) returns different things for x and y:
+
+print "\nA to Z: "
+('a'..'z').each { |letter| print letter }
+
+print "\n0 to 5: "
+(0..5).each { |number| print number }
+
+print "\n0 to 4: "
+(0...5).each { |number| print number }
+puts ''
+
+# Arrays & Hashes
+some_array = ['one', 'more', 'time']
+some_array.each { |x| puts x }
+some_hash = { 'o' => 'one', 'm' => 'more', 't' => 'time' }
+some_hash.each { |k, v| puts "Key #{k}, value #{v}" }
+
+exit
