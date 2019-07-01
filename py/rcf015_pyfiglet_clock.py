4771ee806248b0aee74db351bf1f14996074319e
Ryan Fleck <ryan.fleck@protonmail.com>
2019-06-30 23:12:51 -0400

    Write a fun clock in Python.

---
+++ py/rcf015_pyfiglet_clock.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# A simple clock that uses figlet. Quite fun.
+
+import pyfiglet
+import time
+import os
+import sys 
+
+clear_cmd = 'clear'
+f = pyfiglet.Figlet(font='5lineoblique')
+if( 'win' in sys.platform ):
+    clear_cmd = 'cls'
+
+while 1:
+    os.system(clear_cmd)
+    now = time.strftime("%H %M %S", time.localtime())
+    print(f.renderText(now))
+    time.sleep(1)
+
