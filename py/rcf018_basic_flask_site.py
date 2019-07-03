8a4535452d788af7b31ab6d6a9fad5da5e6ad160
Ryan Fleck <ryan.fleck@protonmail.com>
2019-07-03 17:22:01 -0400

    Finish the simple flask site.

--- py/rcf018_basic_flask_site.py
+++ py/rcf018_basic_flask_site.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template_string, request
+import time
 
 app = Flask(__name__)
 
@@ -41,11 +42,44 @@ basepage = '''<!DOCTYPE html>
 
 @app.route('/')
 def hello():
+    debugRequest(request)
     return(render_template_string(
         basepage,
         title="Welcome to my Flask Site",
-        content="Hello, World! The page was generated on "+
+        content="Hello, World! The page was generated on "
+        + time.strftime("%A %B %-d at %H:%M:%S (%z).", time.localtime())
+        + " Flask is pretty neat. It allows you to make tiny, simple web"
+        + " applications in a quick, clean and easily-deployable manner."
     ))
 
+@app.route('/<string:name>')
+def levelone(name):
+    debugRequest(request)
+    return(render_template_string(
+        basepage,
+        title=name.strip().title(),
+        content="Welcome to the "+name.strip().title()+" page, which was generated on "
+        + time.strftime("%A %B %-d at %H:%M:%S (%z).", time.localtime())
+        + " Flask is pretty neat. It allows you to make tiny, simple web"
+        + " applications in a quick, clean and easily-deployable manner."
+    ))
+    
+
+@app.errorhandler(404)
+def couldnt_find_page(e):
+    debugRequest(request)
+    response=''
+    errored_page = request.args.get('url')
+    if( errored_page ):
+        response = 'The page '+errored_page+' does not exist.'
+    return(render_template_string(
+        basepage,
+        title="LOL, 404!",
+        content= response + str(e)
+    ))
+
+def debugRequest(request):
+    print( request )
+    print( request.get_json() )
 
 app.run()
