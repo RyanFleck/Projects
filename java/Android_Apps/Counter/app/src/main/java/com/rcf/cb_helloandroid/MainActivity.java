fe2d8811c11dc5905da6ba450675ef15fe060e13
Ryan Fleck <ryan.fleck@protonmail.com>
2019-10-04 16:47:33 -0400

    Counter persists between app closures.

--- java/Android_Apps/Counter/app/src/main/java/com/rcf/cb_helloandroid/MainActivity.java
+++ java/Android_Apps/Counter/app/src/main/java/com/rcf/cb_helloandroid/MainActivity.java
@@ -1,8 +1,7 @@
 package com.rcf.cb_helloandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -13,12 +12,29 @@ public class MainActivity extends Activity {
 
     private int x = 0;
     private TextView tv;
+    private SharedPreferences prefs;
+    private String storage_key = "count-value";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Hide decorations, clear status bar color.
         getWindow().setStatusBarColor(Color.BLACK);
         setContentView(R.layout.activity_main);
+
+        prefs = getApplicationContext().getSharedPreferences("cb-counter",0);
+
+
+
+        if(prefs.contains(storage_key)){
+            x = prefs.getInt(storage_key,0);
+            Toast.makeText(this, "Counter has val "+x, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "New counter.", Toast.LENGTH_SHORT).show();
+        }
+
+        // Set textview, update text.
         tv = findViewById(R.id.upper_text);
         updateText();
     }
@@ -40,6 +56,17 @@ public class MainActivity extends Activity {
         updateText();
     }
 
+    public void set(int y){
+        x = y;
+
+        // X cannot go below zero.
+        if (x < 0) {
+            x = 0;
+        }
+
+        updateText();
+    }
+
     private void updateText() {
         tv.setText(String.valueOf(x));
 
@@ -49,5 +76,8 @@ public class MainActivity extends Activity {
         } else {
             findViewById(R.id.button_lower).setVisibility(View.INVISIBLE);
         }
+
+        prefs.edit().putInt(storage_key,x).apply();
+
     }
 }
