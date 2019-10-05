444a80f4704db258d5c4c58ed03bbfec1deca6e8
Ryan Fleck <ryan.fleck@protonmail.com>
2019-10-05 08:39:55 -0400

    Learn how to create scrolling debug view.

--- java/Android_Apps/DebugScrollView/app/src/main/java/com/rcf/debugscrollview/MainActivity.java
+++ java/Android_Apps/DebugScrollView/app/src/main/java/com/rcf/debugscrollview/MainActivity.java
@@ -3,6 +3,10 @@ package com.rcf.debugscrollview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get main linear layout.
+        LinearLayout x = findViewById(R.id.main_linear_layout);
+
+        for(int i=1; i<=100; i++){
+            TextView y = new TextView(this);
+            String text = Calendar.getInstance().getTime().toString()+" I'm #"+i;
+            y.setText(text);
+            x.addView(y);
+        }
+
+
     }
 }
