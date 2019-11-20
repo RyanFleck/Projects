d887f208c1c9fa7ba4c81673f2f3543d3f748d12
Ryan Fleck <ryan.fleck@protonmail.com>
2019-11-20 14:10:27 -0500

    Add degree symbol, error code detection.

--- py/rcf004_boids_two.py
+++ py/rcf004_boids_two.py
@@ -5,8 +5,12 @@ apt install libglfw3
 pip3 install p5 numpy
 '''
 
-from p5 import stroke, circle, setup, draw, size, background, run
+from p5 import stroke, circle, setup, draw, size, background, run, Vector
 import numpy as np
+
+speedLimit = 9
+width = 600
+height = 400
 
 '''
 Boid brains.
@@ -17,11 +21,38 @@ class Boid:
     '''A boid is a pack animal.'''
 
     def __init__(self, x, y):
-        self.position = {'x': x, 'y': y}
+        self.position = Vector(x, y)
+        self.velocity = Vector(*np.random.rand(2))
+        self.acceleration = Vector(*np.random.rand(2))
 
     def show(self):
         stroke(255)
-        circle((self.position["x"], self.position["y"]), radius=10)
+        circle((self.position.x, self.position.y), radius=10)
+
+    def react(self, *otherBoids):
+        for boid in otherBoids:
+            
+
+    def update(self):
+        self.position += self.velocity
+        self.velocity += self.acceleration
+
+        # Wrap X
+        if self.position.x > width:
+            self.position.x = 0
+        elif self.position.x < 0:
+            self.position.x = width
+
+        # Wrap Y
+        if self.position.y > height:
+            self.position.y = 0
+        elif self.position.y < 0:
+            self.position.y = height
+
+        # Limit Speed
+        if np.linalg.norm(self.velocity) > speedLimit:
+            self.velocity = self.velocity / \
+                np.linalg.norm(self.velocity) * speedLimit
 
 
 '''
@@ -33,8 +64,6 @@ flock = [Boid(x=np.random.rand() * 600, y=np.random.rand() * 400)
 
 
 def setup():
-    width = 600
-    height = 400
     size(width, height)
 
 
@@ -43,12 +72,17 @@ def draw():
 
     for boid in flock:
         boid.show()
+        boid.update()
 
 
 # Run the simulation.
 run()
 
+
 '''
+Tutorial:
+https://medium.com/better-programming/boids-simulating-birds-flock-behavior-in-python-9fff99375118
+
 Useful material:
 https://pythontips.com/2013/08/04/args-and-kwargs-in-python-explained/
 '''
--- py/rcf025_openweathermap.py
+++ py/rcf025_openweathermap.py
@@ -1,14 +1,25 @@
+#!/usr/bin/env python3
+
 import requests
 import json
 import os
 
-key=os.environ.get("OWM_KEY")
+key = os.environ.get("OWM_KEY")
 
 url = "http://api.openweathermap.org/data/2.5/weather"
 city = "Ottawa"
 
 request_string = "{}?appid={}&q={}".format(url, key, city)
 
-response = requests.get(request_string).json()["main"]
+response = requests.get(request_string).json()  # ["main"]
 
-print("Current temperature in {}: {:3.2f}".format(city, response['temp']-273.15))
+# Response code 200 is the correct response.
+if(response["cod"] != 200):
+    print("Error. {}".format(response))
+else:
+    # Temperature is given in Kelvin.
+    print(
+        "Current temperature in {}: {:3.2f}\N{DEGREE SIGN}C".format(
+            city,
+            response['main']['temp'] -
+            273.15))
