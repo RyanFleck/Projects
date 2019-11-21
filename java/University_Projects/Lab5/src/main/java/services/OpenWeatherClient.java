bd697940a97c5f261912572698ebf185fc9399aa
Ryan Fleck <ryan.fleck@protonmail.com>
2019-11-20 22:22:00 -0500

    Write a thing.

---
+++ java/University_Projects/Lab5/src/main/java/services/OpenWeatherClient.java
@@ -0,0 +1,37 @@
+package services;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.core.MediaType;
+import model.WeatherConditions;
+import model.WeatherConditionsFactory;
+
+/**
+ *
+ * @author ssome
+ */
+public class OpenWeatherClient {
+
+    /*
+     * Note to marker- I'm not using my own OpenWeatherMap API key here.
+     */
+    private final String APPID_HEADER = System.getenv("OWM_KEY");
+
+    private final String baseUrl = "http://api.openweathermap.org/data/2.5/";
+    private final String resource = "weather";
+    private final Client client;
+
+    public OpenWeatherClient() {
+        client = javax.ws.rs.client.ClientBuilder.newClient();
+    }
+
+    public WeatherConditions currentWeatherAtCity(String cityName, String countryCode) throws Exception {
+        String jsonResponse = client.target(baseUrl)
+                .path(resource)
+                .queryParam("q", cityName + "," + countryCode)
+                .queryParam("appid", APPID_HEADER)
+                .request(MediaType.APPLICATION_JSON)
+                .get(String.class);
+        return WeatherConditionsFactory.create(jsonResponse);
+    }
+
+}
