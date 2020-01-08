625e38b5ee5bcc9e78b77041d4207b446fa2308c
Ryan Fleck <ryan.fleck@protonmail.com>
2020-01-08 06:48:43 -0500

    Replace CV PDF

---
+++ java/University_Projects/Influx/src/main/java/com/rcf/seg3102/influx/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.rcf.seg3102.influx;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
