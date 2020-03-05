49b2074794f35311494afaed7ad2e1ec788a6ea6
Ryan Fleck <ryan.fleck@protonmail.com>
2020-03-05 17:44:32 -0500

    Update java doodle.

--- java/University_Projects/PatternPrinter.java
+++ java/University_Projects/PatternPrinter.java
@@ -5,6 +5,9 @@ class PatternPrinter {
     public static void main(String[] args) {
         System.out.println("Pattern Printer V1.\nGenerating...");
         patternOne();
+        patternTwo();
+        patternThree();
+        patternFour();
     }
 
     public static ArrayList<String> patternOne() {
@@ -29,7 +32,88 @@ class PatternPrinter {
                 System.out.println(s.toString());
             }
         }
+        gap();
         return pattern;
+    }
+    public static ArrayList<String> patternTwo() {
+        ArrayList<String> pattern = new ArrayList<String>();
+
+        for(int i=1; i<=9; i++) {
+            if(i==1 || i==5 || i==9) {
+                String line;
+                line = repeatString(Integer.toString(i), 18);
+                System.out.println(line);
+            } else {
+                StringBuilder s = new StringBuilder();
+                s.append(Integer.toString(i));
+                for(int j=1; j<=8; j++) {
+                    s.append(".");
+                }
+                s.append(Integer.toString(i));
+                for(int j=1; j<=8; j++) {
+                    s.append(".");
+                }
+                s.append(Integer.toString(i));
+                System.out.println(s.toString());
+            }
+        }
+        gap();
+        return pattern;
+    }
+
+    public static ArrayList<String> patternThree() {
+        ArrayList<String> pattern = new ArrayList<String>();
+
+        for(int i=1; i<=9; i++) {
+            if(i==1 || i==9) {
+                String line;
+                line = repeatString(Integer.toString(i), 18);
+                System.out.println(line);
+            } else {
+                StringBuilder s = new StringBuilder();
+                s.append(Integer.toString(i));
+                for(int j=1; j<=8; j++) {
+                    if( j%3 == 0 ) {
+                        s.append("x");
+                    } else {
+                        s.append(".");
+                    }
+                }
+                s.append(Integer.toString(i));
+                for(int j=1; j<=8; j++) {
+                    if( j%3 == 0 ) {
+                        s.append("x");
+                    } else {
+                        s.append(".");
+                    }
+                }
+                s.append(Integer.toString(i));
+                System.out.println(s.toString());
+            }
+        }
+        gap();
+        return pattern;
+    }
+
+    public static ArrayList<String> patternFour() {
+        ArrayList<String> pattern = new ArrayList<String>();
+        int instances = 0;
+
+        for(int i=1; i<=9; i++) {
+            int dotnum = 9 - i;
+            StringBuilder s = new StringBuilder();
+            s.append(repeatString(".",dotnum));
+            s.append(repeatString(Integer.toString(i),instances));
+            s.append(repeatString(".",dotnum));
+            System.out.println(s.toString());
+            instances += 2;
+        }
+        gap();
+        return pattern;
+    }
+
+    public static void gap() {
+        System.out.println("");
     }
 
     public static String repeatString(String x, int n) {
